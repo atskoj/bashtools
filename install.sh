8eea4796ee3e2d1c050f7ca65c308df307e28fda
atskoj <chastonmax@gmail.com>
2020-12-06 04:21:54 +0000

    added install.sh file

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+chmod +x cpuclock/cpuclock.sh
+mv cpuclock/cpuclock.sh /usr/local/bin/cpuclock
+
+chmod +x mouselocation/mouselocation.sh
+mv mouselocation/mouselocation.sh /usr/local/bin/mouselocation
+
+chmod +x performance/performance.sh
+mv performance/performance.sh /usr/local/bin/performance
+
+chmod +x time/time.sh
+mv time/time.sh /usr/local/bin/time
+
+chmod +x powersave/powersave.sh
+mv powersave/powersave.sh /usr/local/bin/powersave
+
